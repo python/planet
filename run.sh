33068886bff14aff1de28982096d78516cd1f7ad
Benjamin Peterson <benjamin@python.org>
2014-09-19 04:28:55 +0000

    updates for new host and move to git

--- run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-cd /data/planet && exec python /data/planet/code/planet.py "$@" 2>/dev/null
-	
--- update-svn.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Script to update the Planet configuration automatically
-
-cd /data/planet/config
-svn up >/dev/null
