6075cd9bed23da289aa76878017654d34987d936
Ari Lamstein <arilamstein@gmail.com>
2025-03-03 12:56:20 -0800

    If you open the file as just 'w' with an encoding of utf-8 then a lot of the changes are unnecessary. Revert changes made by black.

--- config/sort-ini.py
+++ config/sort-ini.py
@@ -1,50 +1,46 @@
 #!/usr/bin/env python
 
 import sys
-import configparser as ConfigParser
+import configparser
 
 if len(sys.argv) > 1:
     filename = sys.argv[1]
 else:
-    filename = "config.ini"
-
-oconfig = ConfigParser.RawConfigParser()
+    filename = 'config.ini'
+    
+oconfig = configparser.RawConfigParser()
 oconfig.read(filename)
 
 # This part will destroy the configuration if there's a crash while
 # writing the output.  We're in an GIT-controlled directory, so
 # I didn't care enough to fix this.
-with open(filename, "wb") as fd:
+with open(filename, 'w', encoding='utf-8') as fd:
     # Copy of write() code that sorts output by section
     if oconfig._defaults:
         fd.write("[%s]\n" % DEFAULTSECT)
-        for key, value in oconfig._defaults.items():
-            fd.write("%s = %s\n" % (key, str(value).replace("\n", "\n\t")))
+        for (key, value) in oconfig._defaults.items():
+            fd.write("%s = %s\n" % (key, str(value).replace('\n', '\n\t')))
         fd.write("\n")
-
+    
     result = {}
     for section in sorted(oconfig._sections):
-        if section == "Planet":
-            fd.write(b"[%s]\n" % section.encode("utf-8"))
-        for key, value in oconfig._sections[section].items():
+        if section == 'Planet':
+            fd.write("[%s]\n" % section)
+        for (key, value) in oconfig._sections[section].items():
             if key != "__name__":
-                if section == "Planet":
-                    fd.write(
-                        b"%s = %s\n"
-                        % (
-                            key.encode("utf-8"),
-                            str(value).replace("\n", "\n\t").encode("utf-8"),
-                        )
-                    )
+                if section == 'Planet':
+                    fd.write("%s = %s\n" %
+                         (key, str(value).replace('\n', '\n\t')))
                 else:
-                    result[value.replace('"', "")] = section
-        if section == "Planet":
-            fd.write("\n".encode("utf-8"))
-
+                    result[value.replace('"', '')] = section
+        if section == 'Planet':
+            fd.write("\n")
+    
     for key, value in sorted(result.items()):
-        fd.write(b"[%s]\n" % value.encode("utf-8"))
+        fd.write("[%s]\n" % value)
         name = key
         if "'" in key:
             name = '"%s"' % key
-        fd.write(b"name = %s\n" % name.encode("utf-8"))
-        fd.write(b"\n")
+        fd.write("name = %s\n" % name)
+        fd.write("\n")
+
